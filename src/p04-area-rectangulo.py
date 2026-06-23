ff22392b75b0449b05d96c11a4ce3862f6f841fc
Carlos Soto <odoo.soporte@gmail.com>
2026-06-23 02:12:25 +0000

    refactor: manejo de errores

--- src/p04-area-rectangulo.py
+++ src/p04-area-rectangulo.py
@@ -13,10 +13,13 @@ print( "--------------------------------- \n" )
 base = input( "Ingrese la base del rectángulo: ")
 altura = input( "Ingrese la altura del rectángulo: ")
 
-if base.isdigit() and altura.isdigit():
+# Manejo de errores
+# Manejar errores con esta estructura
+# tiene un costo
+try:
     area = int( base ) * int( altura )
     print( f"\nEl área del rectángulo es: {area}" )
-else:
+except ValueError:
     print( "ERROR" )
     print( "Los valores ingresados deben ser número enteros" )
     print( f"Valores ingresados Base: {base} - Altura: {altura}" )
