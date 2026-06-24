d1545542424731c0c250ab408da858fd0e8cb2c5
Carlos Soto <odoo.soporte@gmail.com>
2026-06-24 23:20:18 +0000

    add: operador condicional

---
+++ src/p05-operadores-comparacion.py
@@ -0,0 +1,10 @@
+# Declaración de variables
+nombre = "Carlos"
+edad = 35
+edad_minima = 18
+
+# ¿La edad de la persona es igual a la edad mínima?
+print( f"¿La edad es igual a la edad mínima? {edad == edad_minima} " )
+
+# Saber si la persona es mayor de edad
+print( f"¿La persona es mayor de edad? {edad >= edad_minima} " )
