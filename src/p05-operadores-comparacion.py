6fb50467455ae1b1258992196a5bc31098c2684b
Carlos Soto <odoo.soporte@gmail.com>
2026-06-24 23:45:56 +0000

    wip: perfil del estudiante

--- src/p05-operadores-comparacion.py
+++ src/p05-operadores-comparacion.py
@@ -8,3 +8,28 @@ print( f"¿La edad es igual a la edad mínima? {edad == edad_minima} " )
 
 # Saber si la persona es mayor de edad
 print( f"¿La persona es mayor de edad? {edad >= edad_minima} " )
+
+#-- - ---------------------------------------------
+#-- - Nuevo contexto
+#-- - Las variables anteriores las ignoramos
+#-- - ---------------------------------------------
+print("\n")
+print("Perfil del estudiante")
+print("---------------------\n")
+
+# Suponemos que esta información llega desde bbdd (PostgreSQL)
+id = 1500
+nombre = "Camila"
+edad = 20
+promedio = 5.8
+asistencia = 82
+
+es_mayor_edad = (edad >= 18)            # Por buena práctica utilizamos paréntesis
+tiene_buen_promedio = promedio >= 5.5   # Pero no son requeridos
+cumple_asistencia = ( asistencia >= 80 )
+
+print( f"El estudiante: {nombre}" )
+print( f"¿Es mayor de edad? {es_mayor_edad}" )
+print( f"¿Tiene buen promedio? {tiene_buen_promedio}" )
+print( f"¿Cumple con la asistencia: {cumple_asistencia}" )
+
