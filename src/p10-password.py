2d96078f553de961fa0ff8ff92878ef56adb0ea0
Carlos Soto <odoo.soporte@gmail.com>
2026-06-25 23:58:43 +0000

    wip: contraseña encriptado

--- src/p10-password.py
+++ src/p10-password.py
@@ -1,7 +1,16 @@
 # getpass es una librería de la biblioteca estandar de Python
 import getpass
+import hashlib
+import hmac
 
 PASS_BBDD = "hola mundo"     # Esto debe estar encriptado
+
+# Contraseña encriptada
+# Así se guarda y recupera de la BBDD
+PASS_HASH = hashlib.sha256(PASS_BBDD.encode()).hexdigest()
+
+print( PASS_HASH )
+
 
 # Para trabajar con password es mala práctica utilizar
 # input() debido a que podemos ver la clave que
@@ -9,7 +18,10 @@ PASS_BBDD = "hola mundo"     # Esto debe estar encriptado
 #password = input("Ingrese la clase secreta: ")
 password = getpass.getpass("Ingrese la clase secreta: ")
 
-while password != PASS_BBDD:
+while not hmac.compare_digest(
+    hashlib.sha256(password.encode()).hexdigest(),
+    PASS_HASH
+):
     password = getpass.getpass("Ingrese la clase secreta: ")
 
 print( "Clave correcta. Puede utilizar el programa" )
