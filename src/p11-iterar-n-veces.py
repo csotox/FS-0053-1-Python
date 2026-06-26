7a54c796380e9ca48ec6b47d519c11a9e0083159
Carlos Soto <odoo.soporte@gmail.com>
2026-06-26 00:28:05 +0000

    add: fundamentos while

---
+++ src/p11-iterar-n-veces.py
@@ -0,0 +1,11 @@
+
+# Inicialización de variables
+# Este tipo de variables se conoce como `contador`
+i = 0
+
+# Sabemos que while se ejecuta mientras la condición sea verdadera
+# Siempre debemos tener un seguro dentro del ciclo while para
+# que no sea un ciclo infinito
+while i < 10:
+    print( f"Esto se mostrará 10 veces. Paso {i}" )
+    i = i + 1
