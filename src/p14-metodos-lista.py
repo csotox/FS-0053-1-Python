0a986a7b8366ec4b89fca42f80b1f226992570a8
Carlos Soto <odoo.soporte@gmail.com>
2026-06-30 23:05:30 +0000

    wip: funcion help

--- src/p14-metodos-lista.py
+++ src/p14-metodos-lista.py
@@ -1,9 +1,11 @@
 # dunder methods
 lista = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-print( lista.__dir__() )
+# print( lista.__dir__() )
 
-# Tamaño del objeto
-print( len(lista) )
-print( lista.__len__() )
+# # Tamaño del objeto
+# print( len(lista) )
+# print( lista.__len__() )
 
+# Mustra una pequeña ayuda del método
+help( lista.pop )
