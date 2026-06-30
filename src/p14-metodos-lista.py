6f30f3acf833d17a81fd5e876ebd7c0722c23087
Carlos Soto <odoo.soporte@gmail.com>
2026-06-30 23:25:09 +0000

    wip: list insert y append

--- src/p14-metodos-lista.py
+++ src/p14-metodos-lista.py
@@ -8,4 +8,39 @@ lista = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # print( lista.__len__() )
 
 # Mustra una pequeña ayuda del método
-help( lista.pop )
+# help( lista.pop )
+
+print( "Método append()" )
+print( "---------------" )
+
+# Inicialización de variable
+colores = []
+
+# Agregamos valores a la lista.
+# Normalmente esto queda
+# dentro de un for
+colores.append('rojo')
+colores.append('rosa')
+colores.append('azul')
+
+print( colores )
+
+print()
+print( "Método insert()" )
+print( "---------------" )
+
+lista_puntos = [ ".", ".", ".", ".", "." ]
+print( lista_puntos )
+
+#   0    1    2    3    4      índice
+# ['.', '.', '.', '.', '.']
+
+lista_puntos.insert(2, "-")
+print( lista_puntos )
+
+#   0    1    2    3    4    5    índice
+# ['.', '.', '-', '.', '.', '.']
+
+lista_puntos.insert(-1, "*")
+print( lista_puntos )
+
