4e9bededeebda8f1b146e3198ca14188af2c56d5
Carlos Soto <odoo.soporte@gmail.com>
2026-07-01 23:36:10 +0000

    ref(p18): agregar funcion es_entero y msj_error

--- src/p18-sumar-dos-numeros.py
+++ src/p18-sumar-dos-numeros.py
@@ -1,8 +1,35 @@
 # Código orginal: src/sumar-dos-numeros.py
 # Hacemos que trabaje con funciones
+# Refactorización
+# - Validación del valor ingresado
+# - Mensaje de error
 
+# La función valida si un valor es de tipo entero (int)
+# Retorno tipo booleano
+#  - Entero = True
+def es_entero(valor):
+    return valor.isdigit()
+
+
+# Muestra un mensaje de error en consola
+# texto = Posición de ingreso del valor
+# valor_error = El valor que da error
+def msj_error(texto, valor_error):
+    print("Error!!!!")
+    print("Los valores ingresados deben ser números enteros")
+    print(f"{texto} número es: [{valor_error}]")
+
+
+# -- - ----------
 numero1 = input("Ingrese el primer número entero:")
 numero2 = input("Ingrese el segundo número entero:")
+
+if not es_entero(numero1):
+    msj_error("Primero", numero1)
+
+if not es_entero(numero2):
+    msj_error("Segundo", numero2)
+
 
 if numero1.isdigit() and numero2.isdigit():
     numero_1 = int(numero1)
@@ -11,7 +38,3 @@ if numero1.isdigit() and numero2.isdigit():
     suma = numero_1 + numero_2
 
     print(f"La suma de los números es: {suma}")
-else:
-    print("Error!!!!")
-    print("Los valores ingresados deben ser números enteros")
-    print(f"Primer número es: {numero1} y segundo número es: {numero2}")
