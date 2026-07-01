f90979770c625cbe8035df36a86aca3eb4664cd1
Carlos Soto <odoo.soporte@gmail.com>
2026-07-01 00:30:02 +0000

    wip(dict): Agregar modificar elemento

---
+++ src/p15-metodos-dict.py
@@ -0,0 +1,22 @@
+print()
+print( "Agregar elementos a un diccionario (dict)" )
+print( "-----------------------------------------" )
+
+# Cuando hacemos referencia a la clave de un dict
+# que no existe, la agrega
+
+diccionario = { "llave 1": 5 }
+
+print( diccionario )
+
+diccionario["llave 2"] = 9
+
+print( diccionario )
+
+print()
+print( "Cambiar valor de una clave de un diccionario (dict)" )
+print( "---------------------------------------------------" )
+
+diccionario["llave 2"] = 18
+
+print( diccionario )
