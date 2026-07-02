74509b1b0c6a5fe2af1f43069084c0be3ac967da
Carlos Soto <odoo.soporte@gmail.com>
2026-07-02 00:25:56 +0000

    ref(p19): Agregada funcion imprimir_menu

--- src/p19-mini-encuesta.py
+++ src/p19-mini-encuesta.py
@@ -1,3 +1,14 @@
+# Mostrar menú por consola
+def imprimir_menu():
+    print('Opciones: ')
+    print('1). De acuerdo')
+    print('2). En desacuerdo')
+    print('3). No me interesa')
+
+# Documentar función
+# Retorno de una función None
+
+
 preguntas = [
     "Enunciado Pregunta 1",
     "Enunciado Pregunta 2",
@@ -7,24 +18,15 @@ preguntas = [
 respuestas = []
 
 print(preguntas[0])
-print('Opciones: ')
-print('1). De acuerdo')
-print('2). En desacuerdo')
-print('3). No me interesa')
+imprimir_menu()
 respuestas.append(input('> '))
 
 print(preguntas[1])
-print('Opciones: ')
-print('1). De acuerdo')
-print('2). En desacuerdo')
-print('3). No me interesa')
+imprimir_menu()
 respuestas.append(input('> '))
 
 print(preguntas[2])
-print('Opciones: ')
-print('1). De acuerdo')
-print('2). En desacuerdo')
-print('3). No me interesa')
+imprimir_menu()
 respuestas.append(input('> '))
 
 print( f"La respuesta a la pregunta 1 es: {respuestas[0]}")
