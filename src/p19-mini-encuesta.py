99991daaeb820812e2e0481a300279aa48c82e75
Carlos Soto <odoo.soporte@gmail.com>
2026-07-02 00:43:40 +0000

    ref(p19): documentacion de la funcion

--- src/p19-mini-encuesta.py
+++ src/p19-mini-encuesta.py
@@ -1,13 +1,18 @@
 # Mostrar menú por consola
-def imprimir_menu():
+def imprimir_menu(x: str = "Valor predeterminado") -> None:
+    """
+    Muestra menú de opciones en la consola
+    """
+    print(x)
     print('Opciones: ')
     print('1). De acuerdo')
     print('2). En desacuerdo')
     print('3). No me interesa')
 
-# Documentar función
-# Retorno de una función None
-
+    # Python siempre regresa un valor desde una función
+    # En el caso de no ser especificado, Pyhton retorna
+    # None
+    # return 1
 
 preguntas = [
     "Enunciado Pregunta 1",
@@ -18,11 +23,11 @@ preguntas = [
 respuestas = []
 
 print(preguntas[0])
-imprimir_menu()
+prueba = imprimir_menu()
 respuestas.append(input('> '))
 
 print(preguntas[1])
-imprimir_menu()
+imprimir_menu("Hola x")
 respuestas.append(input('> '))
 
 print(preguntas[2])
@@ -32,3 +37,4 @@ respuestas.append(input('> '))
 print( f"La respuesta a la pregunta 1 es: {respuestas[0]}")
 print( f"La respuesta a la pregunta 2 es: {respuestas[1]}")
 print( f"La respuesta a la pregunta 3 es: {respuestas[2]}")
+print( f"El valor devuelto por la Fx es: {prueba}")
