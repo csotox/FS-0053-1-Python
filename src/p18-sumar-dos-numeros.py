be4a264760543e7f12a0a6364527da4ab10d1b34
Carlos Soto <odoo.soporte@gmail.com>
2026-07-02 00:09:00 +0000

    ref(p18): agregar funcion sumar

--- src/p18-sumar-dos-numeros.py
+++ src/p18-sumar-dos-numeros.py
@@ -1,8 +1,12 @@
+import sys
+
 # Código orginal: src/sumar-dos-numeros.py
 # Hacemos que trabaje con funciones
 # Refactorización
 # - Validación del valor ingresado
 # - Mensaje de error
+# - Funcion sumar
+
 
 # La función valida si un valor es de tipo entero (int)
 # Retorno tipo booleano
@@ -20,21 +24,25 @@ def msj_error(texto, valor_error):
     print(f"{texto} número es: [{valor_error}]")
 
 
+def sumar(n1, n2):
+    return n1 + n2
+
+
 # -- - ----------
 numero1 = input("Ingrese el primer número entero:")
 numero2 = input("Ingrese el segundo número entero:")
 
 if not es_entero(numero1):
     msj_error("Primero", numero1)
+    sys.exit()
 
 if not es_entero(numero2):
     msj_error("Segundo", numero2)
+    sys.exit()
 
+numero_1 = int(numero1)
+numero_2 = int(numero2)
 
-if numero1.isdigit() and numero2.isdigit():
-    numero_1 = int(numero1)
-    numero_2 = int(numero2)
+suma = sumar(numero_1, numero_2)
 
-    suma = numero_1 + numero_2
-
-    print(f"La suma de los números es: {suma}")
+print(f"La suma de los números es: {suma}")
