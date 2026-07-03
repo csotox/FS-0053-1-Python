e42cfb26c8bcbeba0a0c2b1777dd9cdb1307546c
Carlos Soto <odoo.soporte@gmail.com>
2026-07-03 02:25:16 +0000

    update: manejar str en funcion sumar

--- src/calculadora_basica/test.py
+++ src/calculadora_basica/test.py
@@ -10,10 +10,33 @@ else:
 
 # Sumar envio de un entero y una cadena
 try:
-    resp = sumar(10, "20") # type: ignore
-    print("E", end="")
+    resp = sumar(10, "20")
+    if resp == 30:
+        print(".", end="")
+    else:
+        print("E", end="")
 except:
-    print(".", end="")
+    print("E", end="")
+
+# Sumar envio de un cadena y un entero
+try:
+    resp = sumar("10", 20)
+    if resp == 30:
+        print(".", end="")
+    else:
+        print("E", end="")
+except:
+    print("E", end="")
+
+# Sumar envio dos cadenas
+try:
+    resp = sumar("10", "20")
+    if resp == 30:
+        print(".", end="")
+    else:
+        print("E", end="")
+except:
+    print("E", end="")
 
 # Restar envio dos números enteros
 resp = restar(10, 20)
@@ -30,3 +53,15 @@ except:
     print(".", end="")
 
 print()
+
+# x = 10
+# print( isinstance(x, int) )     # true
+# print( type(x) == int )         # true
+# print( type(x) == "int" )       # false
+# print( x == int )               # false
+
+# if type(x) == int:
+#     print( "es entero")
+
+# r = type(x) == int
+# print( r )
