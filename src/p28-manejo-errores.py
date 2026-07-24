bdfdef728eb96c10cc78f5c84b27ab0a330c7362
Carlos Soto <odoo.soporte@gmail.com>
2026-07-24 00:24:00 +0000

    wip(p28): tipos de errores

---
+++ src/p28-manejo-errores.py
@@ -0,0 +1,45 @@
+
+# -- -----------------------------------
+# Tres tipos de errores en proigramación
+# -- -----------------------------------
+
+# Errores de sintaxis
+# -- ----------------
+# Estos errores impiden la ejecución del programa
+# cuando son detectados
+
+# edad = 20
+
+# if edad >= 18:
+# print("Eres mayor de edad")
+
+
+# Errores en tiempo de ejecución
+# -- ---------------------------
+# Ocurren durante la ejecución del programa
+# cuando queremos realizar una operación
+# no valida
+
+# n1 = 10
+# n2 = 0
+
+# r = n1 / n2
+
+
+# Errores de lógica
+# -- ---------------
+# El programa se ejecuta sin detenerse, pero el resultado
+# es incorrecto o no esperado.
+
+# precio = 10_000
+# descuento = 20
+
+# precio_venta = precio - descuento
+
+# print(f"El precio de venta es: {precio_venta:,}")
+
+
+
+
+
+
