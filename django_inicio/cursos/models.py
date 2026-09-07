684e5fc99fc7c8cbe641dd1afe20321cb00df710
Carlos Soto <odoo.soporte@gmail.com>
2026-09-07 23:55:04 +0000

    wip(django): add model Clasif

--- django_inicio/cursos/models.py
+++ django_inicio/cursos/models.py
@@ -15,6 +15,22 @@ class Cursos(models.Model):
     def __str__(self) -> str:
         return self.nombre
 
+# Modelo para clasificar cursos
+class Clasi(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    uuid = models.UUIDField(
+        unique=True,
+        editable=False,
+        default=uuid.uuid4
+    )
+
+    nombre = models.CharField( max_length=50 )
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
 """
 Ciclo de vida
 -------------
