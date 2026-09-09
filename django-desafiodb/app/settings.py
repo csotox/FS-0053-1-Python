a9d74af525f1f0acbc00467be9d9b99414d8ff6b
Carlos Soto <odoo.soporte@gmail.com>
2026-09-09 22:40:24 +0000

    desafio(0701): configuracion inicial env en settings

--- django-desafiodb/app/settings.py
+++ django-desafiodb/app/settings.py
@@ -10,11 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv( BASE_DIR / '.env', override=True )
 
+print('-- - --')
+print( BASE_DIR )
+print( os.getenv('POSTGRE_DB', 'False') )
+print( os.getenv('SALUDO', 'False') )
+print('-- - --')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
@@ -75,9 +83,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "adl-test",          # <-- Base de datos del sistema
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
 
