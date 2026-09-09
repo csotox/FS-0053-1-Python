7ffcb456ee186f504e3f9735b6f80fa086067fd2
Carlos Soto <odoo.soporte@gmail.com>
2026-09-09 22:58:33 +0000

    desafio(0701): crear tabla con nombre especifico

--- django-desafiodb/testdb/models.py
+++ django-desafiodb/testdb/models.py
@@ -7,3 +7,14 @@ class Adltest(models.Model):
 
     def __str__(self):
         return self.campo1
+
+class AdlEjemplo(models.Model):
+    campo1 = models.CharField( max_length=100 )
+    valor1 = models.IntegerField()
+
+    # testdb_adl_ejemplo
+    class Meta:
+        db_table = 'ejemplo_nombre_tabla'
+
+    def __str__(self):
+        return self.campo1
