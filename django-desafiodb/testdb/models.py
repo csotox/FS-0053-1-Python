e602ccf576e101b04b014ddd62e8c22b2c7155bf
Carlos Soto <odoo.soporte@gmail.com>
2026-09-09 22:52:45 +0000

    desafio(0701): agregamos modelo

--- django-desafiodb/testdb/models.py
+++ django-desafiodb/testdb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Adltest(models.Model):
+    campo1 = models.CharField( max_length=100 )
+    valor1 = models.IntegerField()
+
+    def __str__(self):
+        return self.campo1
