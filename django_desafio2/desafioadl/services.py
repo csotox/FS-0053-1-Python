cdea76d7ea51a7facc23f3db47e9e0b179d0f4e7
Carlos Soto <odoo.soporte@gmail.com>
2026-09-10 23:48:22 +0000

    desafio(0702): add services

---
+++ django_desafio2/desafioadl/services.py
@@ -0,0 +1,19 @@
+from desafioadl.models import Tarea, SubTarea
+
+def recupera_tareas_y_sub_tareas():
+    ...
+
+def crear_nueva_tarea():
+    ...
+
+def crear_sub_tarea():
+    ...
+
+def elimina_tarea():
+    ...
+
+def elimina_sub_tarea():
+    ...
+
+def imprimir_en_pantalla():
+    ...
