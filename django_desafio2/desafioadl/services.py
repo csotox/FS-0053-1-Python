5c68f1bd8dc8568f11a0c5e9bd84c9c91c76761f
Carlos Soto <odoo.soporte@gmail.com>
2026-09-11 00:15:14 +0000

    desafio(0702): fix en busqueda con softdelete

--- django_desafio2/desafioadl/services.py
+++ django_desafio2/desafioadl/services.py
@@ -30,7 +30,7 @@ def crear_nueva_tarea(descri: str = ''):
     return recupera_tareas_y_sub_tareas()
 
 def crear_sub_tarea(tarea_id: int = 0, descri: str = ''):
-    tarea = Tarea.objects.get( id = tarea_id )
+    tarea = Tarea.objects.get( id = tarea_id, eliminada=False )
 
     sub_tarea = SubTarea.objects.create(
         descripcion=descri,
